5722d8f2651927de21b75dd3964cdcaba3cbaebe
filip <filip.pandi@inovatrend.com>
2023-04-18 10:33:38 +0200

    Add primereact for build export.

--- pages/components/componentPublic.js
+++ pages/components/componentPublic.js
@@ -1,6 +1,9 @@
 import {Panel} from "primereact/panel";
 import {Button} from "primereact/button";
 import Link from "next/link";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 
 export default function ComponentPublic() {
     return (
